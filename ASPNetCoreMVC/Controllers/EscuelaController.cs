a3a4a6ab396c0feee2789156cf02c14ebf3d0595
Luis Ricardo López Sánchez <ingricardolopezsanchez@gmail.com>
2022-10-05 16:15:41 -0500

    Iniciando repositorio

---
+++ ASPNetCoreMVC/Controllers/EscuelaController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ASPNetCoreMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNetCoreMVC.Controllers
+{
+    public class EscuelaController : Controller
+    {
+        public IActionResult Index()
+        {
+            var escuelas = new Escuela();
+            escuelas.AñoFundación = 2005;
+            escuelas.EscuelaId = Guid.NewGuid().ToString();
+            escuelas.Nombre = "PLATZI SCHOOL";
+            return View(escuelas);
+        }
+    }
+}
---
+++ ASPNetCoreMVC/README.md
@@ -0,0 +1,4 @@
+﻿# ASP .Net Core MVC
+
+Este repositorio tiene como proposito mostrar el desarrollo del proyecto realizado durante el curso de
+ASP .Net Core en Platzi

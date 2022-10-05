ead325c40b4064f7937921107f24eafe07303f89
Luis Ricardo López Sánchez <ingricardolopezsanchez@gmail.com>
2022-10-05 17:58:50 -0500

    Añadiendo vista de asignatura y escuela

---
+++ ASPNetCoreMVC/Controllers/AsignaturaController.cs
@@ -0,0 +1,18 @@
+﻿using ASPNetCoreMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNetCoreMVC.Controllers
+{
+    public class AsignaturaController : Controller
+    {
+        public IActionResult Index()
+        {
+            var asignatura = new Asignatura
+            {
+                UniqueId = Guid.NewGuid().ToString(),
+                Nombre = "Programación"
+            };
+            return View(asignatura);
+        }
+    }
+}
--- ASPNetCoreMVC/Controllers/EscuelaController.cs
+++ ASPNetCoreMVC/Controllers/EscuelaController.cs
@@ -12,6 +12,10 @@ namespace ASPNetCoreMVC.Controllers
             escuelas.AñoDeCreación = 2005;
             escuelas.UniqueId = Guid.NewGuid().ToString();
             escuelas.Nombre = "PLATZI SCHOOL";
+            escuelas.Pais = "México";
+            escuelas.Ciudad = "Ciudad de México";
+            escuelas.TipoEscuela = TiposEscuela.Secundaria;
+            escuelas.Dirección = "México Av. Revolución C.P: 09809";
             return View(escuelas);
         }
     }
